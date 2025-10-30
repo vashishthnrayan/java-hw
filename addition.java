7f1f29b11617537d3c57cf79b889ac8d81514476
vashishthnrayan <vedmedicare123@gmail.com>
2025-10-30 18:55:48 +0530

    addition

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static void main(String[] args) {
+       int a = 706;
+       int b= 907;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+        System.out.println("Thanks for using our addition program!");
+    }
+}
